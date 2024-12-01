50a00ed76166ddf7e720c40b02407824531baa74
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-12-01 20:56:48 +0530

    handleCommand.js

---
+++ functions/handleCommand.js
@@ -0,0 +1,22 @@
+async function handleCommand(interaction) {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true,
+        });
+    }
+}
+
+module.exports = handleCommand;
