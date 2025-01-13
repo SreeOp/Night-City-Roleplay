e24e46508bfa0ac9c95a29d7052c145d7b72c234
SreeOp <134767890+SreeOp@users.noreply.github.com>
2025-01-13 20:04:07 +0530

    fivemStatus.js

--- functions/fivemStatus.js
+++ functions/fivemStatus.js
@@ -52,7 +52,7 @@ module.exports = (client) => {
 
                     const embed = new Discord.EmbedBuilder()
                         .setColor('Purple')
-                        .setDescription('**Server Name**\n```Night City```\n**How to join the server?**\nYou can join the server **Night City** using our IP: ```connect nrp.coreplex.in```. Live server status can be tracked below!!')
+                        .setDescription('**Server Name**\n```Night City```\n**How to join the server?**\nYou can join the server **Night City** using our IP: ```connect nrp.coreplex.in```')
                         .addFields(
                             { name: 'Server Status', value: '```Online ✅```', inline: true },
                             { name: 'Online Players', value: `\`\`\`${dynamicData.clients}/${dynamicData.sv_maxclients}\`\`\``, inline: true },
